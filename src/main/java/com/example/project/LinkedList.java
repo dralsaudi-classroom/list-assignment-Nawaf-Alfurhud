be213b63ca3b097d8c357b69c8342e1f8f310a51
Nawaf-Alfurhud <Nawaf1alfurhud@gmail.com>
2024-09-26 21:26:04 +0300

    Update LinkedList.java

--- src/main/java/com/example/project/LinkedList.java
+++ src/main/java/com/example/project/LinkedList.java
@@ -55,24 +55,29 @@ public class LinkedList<T> implements List<T>{
         }
     }
     public T mostFrequentElement() {
-        T mfe = null ;
-in t max = 0;
-Node <T > p = head ;
-while ( p != null ) {
-Node <T > q = p ;
-int count = 0;
-while ( q != null ) {
-if ( q . data . equals ( p . data ) )
-count ++;
-q = q . next ;
-}
-i f ( count > max ) {
-max = count ;
-mfe = p . data ;
-}
-p = p . next ;
-}
-return mfe ;
+        T mfe = null;
+int max = 0;
+Node<T> p = head;
 
+while (p != null) {
+    Node<T> q = p;
+    int count = 0;
+
+    while (q != null) {
+        if (q.data.equals(p.data)) {
+            count++;
+        }
+        q = q.next;
     }
+
+    if (count > max) {
+        max = count;
+        mfe = p.data;
+    }
+
+    p = p.next;
+}
+
+return mfe;
+
 }
